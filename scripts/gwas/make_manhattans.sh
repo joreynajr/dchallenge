09063d301e66ffcbe63dba0448ead01722066ff6
joreynajr <joreyna@live.com>
2022-01-11 13:49:40 -0800

    Adding a GWAS plot figure

---
+++ scripts/gwas/make_manhattans.sh
@@ -0,0 +1,6 @@
+/mnt/BioHome/jreyna/software/anaconda3/envs/hic_tls/bin/python \
+    /home/jreyna/jreyna/projects/dchallenge/scripts/gwas/Summarize_the_GWAS_Results.py \
+    /home/jreyna/jreyna/projects/dchallenge/results/main/2021_Nikhil_eQTL/Data/T1D_GWAS/T1D_34012112_Gaulton/GWAS_input_colocalization_pval_lt_5eMinus8.txt \
+    T1D_34012112_Gaulton \
+    /home/jreyna/jreyna/projects/dchallenge/results/main/gwas
+    #/home/jreyna/jreyna/projects/dchallenge/results/main/2021_Nikhil_eQTL/Data/T1D_GWAS/T1D_34012112_Gaulton/GWAS_input_colocalization.txt
