ff6fa5a3d737a930a203ff8a75ec044ce331bab8
joreynajr <joreyna@live.com>
2022-02-15 15:11:11 -0800

    Clean up

--- scripts/coloc/qjobs/temp_script_Coloc_T1D_34012112_Gaulton_DICE_eQTL_TFH.sh
+++ scripts/coloc/qjobs/temp_script_Coloc_T1D_34012112_Gaulton_DICE_eQTL_TFH.sh
@@ -13,5 +13,5 @@ TMPDIR=/scratch
 cd $PBS_O_WORKDIR
 
 source /mnt/BioAdHoc/Groups/vd-ay/jreyna/projects/dchallenge/results/main/2021_Nikhil_eQTL/Code/Colocalization/Activate_R-3.6.1.sh
-Rscript /mnt/BioAdHoc/Groups/vd-ay/jreyna/projects/dchallenge/results/main/2021_Nikhil_eQTL/Code/Colocalization/Colocalization_Analysis_GWAS_Script.R /mnt/BioAdHoc/Groups/vd-ay/jreyna/projects/dchallenge/results/main/2021_Nikhil_eQTL/Data/T1D_GWAS/T1D_34012112_Gaulton/GWAS_input_colocalization_pval_lt_5eMinus8.txt /mnt/BioAdHoc/Groups/vd-ay/jreyna/projects/dchallenge/results/main/2021_Nikhil_eQTL/Results/Colocalization/T1D_34012112_Gaulton/DICE_eQTL_TFH /mnt/BioAdHoc/Groups/vd-ay/jreyna/projects/dchallenge/results/main/2021_Nikhil_eQTL/Data/eqtl_sqtl_summ_stats/DICE_eQTL/TFH.txt.gz
+Rscript /mnt/BioAdHoc/Groups/vd-ay/jreyna/projects/dchallenge/results/main/2021_Nikhil_eQTL/Code/Colocalization/Colocalization_Analysis_GWAS_Script_Generalized.R /mnt/BioAdHoc/Groups/vd-ay/jreyna/projects/dchallenge/results/main/2021_Nikhil_eQTL/Data/T1D_GWAS/T1D_34012112_Gaulton/GWAS_input_colocalization_pval_lt_5eMinus8.txt /mnt/BioAdHoc/Groups/vd-ay/jreyna/projects/dchallenge/results/main/2021_Nikhil_eQTL/Results/Colocalization_Update/T1D_34012112_Gaulton/DICE_eQTL_TFH /mnt/BioAdHoc/Groups/vd-ay/jreyna/projects/dchallenge/results/main/2021_Nikhil_eQTL/Data/eqtl_sqtl_summ_stats/DICE_eQTL/TFH.txt.gz
 
