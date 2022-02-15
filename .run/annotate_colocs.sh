e8f23c86e882d9233b4b79606456ca753951e20c
joreynajr <joreyna@live.com>
2022-02-15 15:13:46 -0800

    Included samplesheet integration for running coloc and annotations

--- .run/annotate_colocs.sh
+++ .run/annotate_colocs.sh
@@ -2,11 +2,21 @@ fns=""
 while IFS= read -r line;
 do
     run_info=($line)
-    fns+="results/main/sgls/${run_info[0]}/${run_info[1]}/${run_info[2]}/${run_info[3]}/ "
-    echo $fns
-    break
-done < <(cat config/sgl.samplesheet.tsv | sed '1d')
+
+    #echo "${run_info[0]}/${run_info[1]}/${run_info[2]}/${run_info[3]}/"
+    fn="results/main/sgls/${run_info[0]}/${run_info[1]}/${run_info[2]}/${run_info[3]}/"
+    dep_fn="results/main/coloc/Results/Colocalization_SMKN/${run_info[0]}/${run_info[1]}/${run_info[2]}"
+    if [[ -d "$dep_fn" ]];
+    then
+        echo "PROCESSING: $fn"
+        fns+="$fn "
+    else
+        echo "WARNING: Missing dependencies for $fn"
+    fi
+done < <(cat config/sgl.samplesheet.tsv.v2 | sed '1d' | grep -v "^#" | head -n 100)
+#done < <(cat config/sgl.samplesheet.tsv | sed '1d')
 
 echo "fns: $fns"
-snakemake --profile workflow/profiles/local/ $@ $fns
+#snakemake --profile workflow/profiles/local/ $@ $fns
+snakemake --profile workflow/profiles/pbs-torque/ $@ $fns
 
--- .run/run_colocalization_eqtl_catalog.sh
+++ .run/run_colocalization_eqtl_catalog.sh
@@ -2,40 +2,58 @@
 # Getting all eQTL datasets
 #########################################################################################
 
-# setting the gwas source 
-#gwas_source="results/main/2021_Nikhil_eQTL/Data/T1D_GWAS/T1D_34012112_Gaulton/GWAS_input_colocalization_pval_lt_5eMinus8.txt"
-gwas_source="T1D_34012112_Gaulton"
+## setting the gwas source 
+##gwas_source="results/main/2021_Nikhil_eQTL/Data/T1D_GWAS/T1D_34012112_Gaulton/GWAS_input_colocalization_pval_lt_5eMinus8.txt"
+#gwas_source="T1D_34012112_Gaulton"
+#
+#skip_outputs=("results/main/coloc/Results/Colocalization_SMKN/T1D_34012112_Gaulton/GENCORD/LCL/" \
+#    "results/main/coloc/Results/Colocalization_SMKN/T1D_34012112_Gaulton/Schmiedel_2018/NK-cell_naive/")
+#
+## running through all the eqtl files
+#outfiles+=""
+#for fn in $(ls results/main/eqtl/*/ge/*_ge_*.all.tsv.gz);
+#do 
+#    # extracting the eqtl_source and ge_source
+#    keynames=$(echo $fn | xargs basename | sed -e "s/_ge_/     /" -e "s/\.all\.tsv\.gz//")
+#    eqtl_source=$(echo $keynames | cut -d " " -f 1)
+#    ge_source=$(echo $keynames | cut -d " " -f 2 | cut -d . -f 1)
+#
+#    #echo "keynames: $keynames"
+#    #echo "eqtl_source: $eqtl_source"
+#    #echo "ge_source: $ge_source"
+#    #echo
+#
+#    # getting the output files
+#    new_fn="results/main/coloc/Results/Colocalization_SMKN/${gwas_source}/${eqtl_source}/${ge_source}/"
+#    if [[ ! " ${skip_outputs[*]} " =~ " ${new_fn} " ]];
+#    then
+#        outfiles+=" $new_fn"
+#        echo "$fn ----------> $new_fn"
+#    fi
+#done
+#
 
-# running through all the eqtl files
-outfiles+=""
-for fn in $(ls results/main/eqtl/*/ge/*_ge_*.all.tsv.gz);
-do 
-    # extracting the eqtl_source and ge_source
-    keynames=$(echo $fn | xargs basename | sed -e "s/_ge_/     /" -e "s/\.all\.tsv\.gz//")
-    eqtl_source=$(echo $keynames | cut -d " " -f 1)
-    ge_source=$(echo $keynames | cut -d " " -f 2 | cut -d . -f 1)
+fns=""
+while IFN= read -r line;
+do
+    run_info=($line)
+    fn="results/main/coloc/Results/Colocalization_SMKN/${run_info[0]}/${run_info[1]}/${run_info[2]}/"
+    fns+="$fn "
 
-    #echo "keynames: $keynames"
-    #echo "eqtl_source: $eqtl_source"
-    #echo "ge_source: $ge_source"
-    #echo
-
-    # getting the output files
-    new_fn="results/main/coloc/Results/Colocalization_SMKN/${gwas_source}/${eqtl_source}/${ge_source}/"
-    if [ $new_fn != "results/main/coloc/Results/Colocalization_SMKN/T1D_34012112_Gaulton/GENCORD/LCL/" ];
-    then
-        outfiles+=" $new_fn"
-        echo "$fn ----------> $new_fn"
-    fi
-done
+done < <(sed '1d' config/coloc_samplesheets/t1d.samplesheet.tsv | grep -v "^#" | head -n 100)
+echo $fns
 
 #outfiles=$(echo $outfiles | cut -d " " -f 4)
-echo
-echo "####################################################################################"
-echo "outfiles: $outfiles"
-echo "####################################################################################"
+#echo
+#echo "####################################################################################"
+#echo "outfiles: $outfiles"
+#echo "####################################################################################"
 
 ##snakemake --profile workflow/profiles/pbs-torque/ -n $outfiles
-snakemake --profile workflow/profiles/pbs-torque/ $@ $outfiles
-#snakemake --profile workflow/profiles/local/ $@ $outfiles
+snakemake --profile workflow/profiles/pbs-torque/ $@ $fns
+#snakemake --profile workflow/profiles/local/ $@ $fns
 #echo "snakemake --profile workflow/profiles/local/ $@ $outfiles"
+
+# ad hoc re-run of NK-cells
+#new_fn="results/main/coloc/Results/Colocalization_SMKN/T1D_34012112_Gaulton/Schmiedel_2018/NK-cell_naive/"
+#snakemake --profile workflow/profiles/pbs-torque/ $@ $new_fn
