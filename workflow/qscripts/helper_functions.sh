99ce277066ecaee2a79415d2f57da7c074ed4c02
joreynajr <joreyna@live.com>
2022-04-26 21:08:09 -0700

    Uploading several qsub scripts for ImmuNext processing and WashU viz

--- workflow/qscripts/helper_functions.sh
+++ workflow/qscripts/helper_functions.sh
@@ -1,5 +1,9 @@
 # setting important paths
 hichip_db_py="/mnt/BioAdHoc/Groups/vd-ay/jreyna/software/mambaforge/envs/hichip-db/bin/python"
+hichip_db_R="/mnt/BioApps/R/3.6.1/bin/R"
+hichip_db_Rscript="/mnt/BioApps/R/3.6.1/bin/Rscript"
+bgzip="/mnt/BioApps/tabix/tabix-0.2.6/bgzip"
+tabix="/mnt/BioApps/tabix/tabix-0.2.6/tabix"
 
 #Setting helper functions
 function path_exists(){
